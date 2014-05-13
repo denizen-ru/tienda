81790e42031f8dd009cb2987da18822aea0244a6
Denis Knyazev <denis.m.knyazev@gmail.com>
2014-05-13 10:10:35 +0400

    settings & wsgi for local developer's server.

--- tienda/settings.py
+++ tienda/settings.py
@@ -62,15 +62,17 @@ WSGI_APPLICATION = 'tienda.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   }
+}
 
-import dj_database_url
-DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+# Heroku db config
+# import dj_database_url
+# DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
